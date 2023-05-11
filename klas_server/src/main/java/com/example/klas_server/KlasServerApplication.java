5f29acab8456b1aea6341d160a3b5da07f161ab7
yeongori <dusrjf14@gmail.com>
2023-05-11 10:31:47 +0900

    [init]

---
+++ klas_server/src/main/java/com/example/klas_server/KlasServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.klas_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlasServerApplication.class, args);
+    }
+
+}
