ab0197306c912a350541bb565c387df10f79d3d9
sangpyeong <miasp1105@naver.com>
2023-06-15 22:27:47 +0900

    [fix]게시판 진행중

--- klas_client/src/pages/Board/BoardPage.js
+++ klas_client/src/pages/Board/BoardPage.js
@@ -14,13 +14,21 @@ function BoardPage() {
       <div className="flex text-[25px] mt-[3%] w-[60%] h-[5%] border-[2px] items-center bg-white rounded-[10px]">
         <div className="ml-4">자유게시판</div>
       </div>
-      <div
-        className="flex text-[15px]  w-[60%] h-[3%] border-[2px] items-center justify-between bg-white rounded-[10px] hover:bg-slate-200 duration-100"
-        onClick={handleToggleForm}
-      >
-        <div className="ml-4">새 글을 작성해주세요!</div>
-        <BsFillPencilFill size="20" className="mr-4" />
-      </div>
+      {showForm === false ? (
+        <div
+          className="flex text-[15px]  w-[60%] h-[3%] border-[2px] items-center justify-between bg-white rounded-[10px] hover:bg-slate-200 duration-100"
+          onClick={handleToggleForm}
+        >
+          <div className="ml-4">새 글을 작성해주세요!</div>
+          <BsFillPencilFill size="20" className="mr-4" />
+        </div>
+      ) : (
+        <div className="flex flex-col text-[15px]  w-[60%] h-[10%] border-[2px] items-center justify-between bg-white rounded-[10px">
+          <div className="flex w-full pr-[10px]">글 제목:</div>
+          <div>내용:</div>
+        </div>
+      )}
+
       <div className="flex flex-col  w-[60%] h-[75%] border-[2px] border-gray-400 bg-white rounded-[10px]">
         <div className="flex flex-col w-full pl-4  h-1/6 justify-evenly hover:bg-slate-200 duration-100">
           <div className="font-bold text-[20px]">교수님 죄송합니다.</div>
