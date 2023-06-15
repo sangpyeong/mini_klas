02beff242f75076f12b52374247f2003e8e34b51
sangpyeong <miasp1105@naver.com>
2023-06-16 00:01:10 +0900

    [fix]카카오 로그인 디자인

--- klas_client/src/pages/User/KakaoLoginPage.js
+++ klas_client/src/pages/User/KakaoLoginPage.js
@@ -5,12 +5,23 @@ import { Link, useLocation } from "react-router-dom";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { ImBubbles3 } from "react-icons/im";
+import { GiCancel } from "react-icons/gi";
 
-function KakaoLoginPage() {
+function KakaoLoginPage({ setModal }) {
   const { userId, setUserId } = useContext(UserContext);
   console.log(userId);
   return (
     <div className="flex flex-col justify-center text-[15px] items-center h-full w-full bg-yellow-200 ">
+      <div className="flex justify-end w-full pr-5 ">
+        <div
+          className="flex "
+          onClick={() => {
+            setModal(false);
+          }}
+        >
+          <GiCancel />
+        </div>
+      </div>
       <div className="flex ">
         <ImBubbles3 size="30" />
         <div className="text-[20px] pl-3 pb-3">카카오로그인</div>
